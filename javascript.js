e94a2e7ff7f27ddd61cd687e8b8b89d75ccddd7d
Dan <dpedersen84@gmail.com>
2018-03-25 16:22:44 -0500

    rating added, few touch ups

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     function gifDisplay() {
 
         var gif = $(this).attr("data-name");
-
+        
         var queryURL =  "https://api.giphy.com/v1/gifs/search?q="
                         + gif + "&api_key=dc6zaTOxFJmzC&limit=10"; 
         //Ajax
@@ -16,6 +16,7 @@ $(document).ready(function() {
             method: "GET"
         })
         .then (function(response) {
+            
             console.log(queryURL);
             console.log(response);
 
@@ -23,17 +24,24 @@ $(document).ready(function() {
 
             //For loop for the results of gif search
             for (var i = 0; i < results.length; i++) {
+
                 var gifDiv = $("<div>");
                 var gifImage = $("<img>");
+                var rating = results[i].rating;
+
+                var gifRating = $("<p>").text("Rating: " + rating);
+
                 //Each image is given below:
                 gifImage.attr("src", results[i].images.fixed_height_still.url);
-                gifImage.attr("data-still", results[i].images.fixed_height_still.url )
-                gifImage.attr("data-animate", results[i].images.fixed_height.url )
-                gifImage.attr("data-state", "still")
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                gifImage.attr("data-state", "still");
+                gifImage.addClass("gif");
 
                 //DOM
-                gifDiv.prepend(gifImage);
                 $("#gifs").prepend(gifDiv);
+                gifDiv.prepend(gifImage);
+                gifDiv.append(gifRating);
             }   
         })
     }
@@ -41,19 +49,21 @@ $(document).ready(function() {
     //Render Default Buttons on Page from the gifArray
     function btnRender () {
         $("#buttons").empty();
+
         //For loop to generate buttons
         for (var i = 0; i < gifArray.length; i++) {
             var b = $("<button>");
-            b.addClass("btn btn-basic btn-gif")
-            b.attr("data-name", gifArray[i])
+            b.addClass("btn btn-basic btn-gif");
+            b.attr("data-name", gifArray[i]);
             b.text(gifArray[i]);
             $("#buttons").append(b);
         }
     }
     btnRender ();
 
+    //Function to animate/freeze gifs
     function gifState () {
-        $(".data-state").on("click"), function () {
+        $(".gif").on("click", function () {
         var state = $(this).attr("data-state");
 
         if(state === "still") {
@@ -65,7 +75,7 @@ $(document).ready(function() {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
-    };
+    });
 };
     //Add Buttons via Search to gifArray
     $("#add-gif").on("click", function() { 
@@ -77,7 +87,7 @@ $(document).ready(function() {
 
     //Click Event to display Gifs
     $(document).on("click", ".btn-gif", gifDisplay);
-    $(document).on("click", ". .data-state", gifState);
+    $(document).on("click", ".gif", gifState);
 
 });//END
 
