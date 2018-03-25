01ae17b6eff05b6aa9e0fcb0550a08fdca2b81ca
Dan <dpedersen84@gmail.com>
2018-03-24 20:26:28 -0500

    added navbar to html, beginning of gif click event

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
         var queryURL =  "https://api.giphy.com/v1/gifs/search?q="
                         + gif + "&api_key=dc6zaTOxFJmzC&limit=10"; 
-
+        //Ajax
         $.ajax({
             url:    queryURL,
             method: "GET"
@@ -25,22 +25,23 @@ $(document).ready(function() {
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 var gifImage = $("<img>");
-                gifImage.addClass("still gif-css")
+                //Each image is given below:
                 gifImage.attr("src", results[i].images.fixed_height_still.url);
-                gifImage.
-                gifImage.addClass("data-animate", results[i].images.fixed_height.url)
-                gifDiv.prepend(gifImage);
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url )
+                gifImage.attr("data-animate", results[i].images.fixed_height.url )
+                gifImage.attr("data-state", "still")
 
+                //DOM
+                gifDiv.prepend(gifImage);
                 $("#gifs").prepend(gifDiv);
-            }
-            
+            }   
         })
     }
             
     //Render Default Buttons on Page from the gifArray
     function btnRender () {
         $("#buttons").empty();
-        //For loop to generate buttons for searches
+        //For loop to generate buttons
         for (var i = 0; i < gifArray.length; i++) {
             var b = $("<button>");
             b.addClass("btn btn-basic btn-gif")
@@ -50,7 +51,22 @@ $(document).ready(function() {
         }
     }
     btnRender ();
-    
+
+    function gifState () {
+        $(".data-state").on("click"), function () {
+        var state = $(this).attr("data-state");
+
+        if(state === "still") {
+            var animatedURL = $(this).attr("data-animate");
+            $(this).attr("src", animatedURL);
+            $(this).attr("data-state", "animate");
+
+        }else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    };
+};
     //Add Buttons via Search to gifArray
     $("#add-gif").on("click", function() { 
         event.preventDefault();
@@ -61,6 +77,7 @@ $(document).ready(function() {
 
     //Click Event to display Gifs
     $(document).on("click", ".btn-gif", gifDisplay);
+    $(document).on("click", ". .data-state", gifState);
 
 });//END
 
